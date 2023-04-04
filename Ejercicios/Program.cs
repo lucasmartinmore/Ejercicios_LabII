782653b225b509b22a3769410a9c26dc929daf44
lucasmartinmore <60674134+lucasmartinmore@users.noreply.github.com>
2023-04-04 00:16:13 -0300

    Add files via upload

---
+++ Ejercicios/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Ejercicios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int acumulador = 0;
+            int numero;
+            char seguir = 'A';
+
+            do 
+            {
+                bool rta = PedirNumero(out  numero);
+                if (rta) 
+                {
+                    acumulador += numero;
+                }
+
+                bool rta2 = DeseaContinuar(out seguir);
+
+            }while(Validador.ValidarRespuesta(seguir));
+
+            Console.WriteLine($"La cantidad total acumulada es de {acumulador}");
+        }
+
+        public static bool PedirNumero(out int numero) 
+        {
+            Console.WriteLine("Ingrese un numero: ");
+            numero = int.Parse(Console.ReadLine()); 
+
+            return true;
+        }
+        
+        public static bool DeseaContinuar(out char respuesta) 
+        {
+            Console.WriteLine("Desea seguir ingresando: ");
+            respuesta = char.Parse(Console.ReadLine()); 
+
+            return true;
+        }
+    }
+}
---
+++ Ejercicios/Validador.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios
+{
+    public class Validador
+    {
+        public static bool ValidarRespuesta(char respuesta)
+        {
+            if(respuesta=='s' || respuesta == 'S')
+            {
+                return true;    
+            }
+            return false;
+        }
+    }
+}
